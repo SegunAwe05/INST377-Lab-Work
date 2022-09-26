21557ab2571fddf09ee2ec4ae44f3159175322ff
Segun Awe <oawe5@umd.edu>
2022-09-25 23:11:21 -0400

    defined constants

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-})
+  const width = 10;
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtm = document.querySelector('#start-button');
+});
